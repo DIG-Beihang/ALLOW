ca51431f43b586842ecd298faf07a3366079ad97
buaali <hainan@buaa.edu.cn>
2023-03-14 09:47:05 +0800

    Update OWOD_ore_split_t1_extending.sh

--- configs/new1026/OWOD_ore_split_t1_extending.sh
+++ configs/new1026/OWOD_ore_split_t1_extending.sh
@@ -10,7 +10,7 @@ python -u main_open_world.py \
   --PREV_INTRODUCED_CLS 0 --CUR_INTRODUCED_CLS 20 \
   --train_set 't1_train' --test_set 'all_task_test' \
   --num_classes 81 \
-  --unmatched_boxes --epochs 70 --top_unk 5 --featdim 1024 --NC_branch --nc_loss_coef 0.1 --nc_epoch 9 \
+  --epochs 70 --featdim 1024 \
   --backbone 'dino_resnet50' \
   --pretrain 'exps/OWOD_t1_ore/checkpoint0049.pth' \
   --cooling_prev 50 \
